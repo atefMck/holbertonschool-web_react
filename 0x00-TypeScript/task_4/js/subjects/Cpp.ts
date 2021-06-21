68567f8a59d84037c594e8119f62b632325b9efc
atefsama666 <mechken.atef@gmail.com>
2021-06-21 15:24:23 +0000

    Task 10

--- 0x00-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x00-TypeScript/task_4/js/subjects/Cpp.ts
@@ -2,7 +2,7 @@ namespace Subjects {
     export interface Teacher {
         experienceTeachingC?: number
     }
-    class Cpp extends Subject {
+    export class Cpp extends Subject {
         getRequirements(): string { return 'Here is the list of requirements for Cpp' }
         getAvailableTeacher(): string { return !this.teacher.experienceTeachingC ?
             'No available teacher' : `Available Teacher: ${this.teacher.firstName}` 
--- 0x00-TypeScript/task_4/js/subjects/Java.ts
+++ 0x00-TypeScript/task_4/js/subjects/Java.ts
@@ -3,7 +3,7 @@ namespace Subjects {
         experienceTeachingJava?: number
     }
     
-    class Java extends Subject {
+    export class Java extends Subject {
         getRequirements(): string { return 'Here is the list of requirements for Java' }
         getAvailableTeacher(): string { return !this.teacher.experienceTeachingJava ? 
             'No available teacher' : `Available Teacher: ${this.teacher.firstName}` 
