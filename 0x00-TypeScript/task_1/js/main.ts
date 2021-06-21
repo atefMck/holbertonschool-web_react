e570350132d5086acdfa8cafcfdf3a9aaf637642
atefsama666 <mechken.atef@gmail.com>
2021-06-21 11:46:52 +0000

    Task 4

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -14,3 +14,31 @@ interface Directors extends Teacher {
 interface printTeacherFunction {
     (firstName: string, lastName: string): void
 }
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+	return `${firstName[0]}. ${lastName}`;
+}
+
+interface StudentClassInterface {
+    firstName: string,
+    lastName: string,
+    workOnHomework(): string,
+    displayName(): string,
+}
+
+interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface
+}
+
+const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string { return 'Currently working' }
+    displayName(): string { return 'Currently working' }
+}
