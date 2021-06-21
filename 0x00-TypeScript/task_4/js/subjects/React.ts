1ebbeeddc5867ad963cbb3c44e72762cdee86d07
atefsama666 <mechken.atef@gmail.com>
2021-06-21 15:13:08 +0000

    Task 4

---
+++ 0x00-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,12 @@
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingReact?: number
+    }
+    
+    class React extends Subject {
+        getRequirements(): string { return 'Here is the list of requirements for React' }
+        getAvailableTeacher(): string { return !this.teacher.experienceTeachingReact ? 
+            'No available teacher' : `Available Teacher: ${this.teacher.firstName}` 
+        }
+    }
+} 
