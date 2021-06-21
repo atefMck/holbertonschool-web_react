38ed2c26bbbfa73c6bad279089e9ad6e92dfed28
atefsama666 <mechken.atef@gmail.com>
2021-06-21 12:46:23 +0000

    task 5

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,28 @@
+interface DirectorInterface {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workDirectorTasks(): string,
+}
+
+interface TeacherInterface  {
+    workFromHome(): string,
+    getCoffeeBreak(): string,
+    workDirectorTasks(): string,
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string { return 'Working from home' }
+    getCoffeeBreak(): string { return 'Getting a coffee break' }
+    workDirectorTasks(): string { return 'Getting to director tasks' }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string { return 'Cannot work from home' }
+    getCoffeeBreak(): string { return 'Cannot have a break' }
+    workDirectorTasks(): string { return 'Getting to work' }
+}
+
+const createEmployee = (salary: string | number): Director | Teacher => {
+    if (typeof salary === "number" && salary < 500) return new Teacher()
+    else return new Director()
+}
