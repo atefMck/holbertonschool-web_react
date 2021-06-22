6407afc323d4b25f7f176106db73b23e656fa2b8
atefsama666 <mechken.atef@gmail.com>
2021-06-22 11:00:24 +0000

    exported necessary classes and functions

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -10,13 +10,13 @@ interface TeacherInterface  {
     workTeacherTasks(): string,
 }
 
-class Director implements DirectorInterface {
+export class Director implements DirectorInterface {
     workFromHome(): string { return 'Working from home' }
     getCoffeeBreak(): string { return 'Getting a coffee break' }
     workDirectorTasks(): string { return 'Getting to director tasks' }
 }
 
-class Teacher implements TeacherInterface {
+export class Teacher implements TeacherInterface {
     workFromHome(): string { return 'Cannot work from home' }
     getCoffeeBreak(): string { return 'Cannot have a break' }
     workTeacherTasks(): string { return 'Getting to work' }
@@ -24,7 +24,7 @@ class Teacher implements TeacherInterface {
 
 
 
-const createEmployee = (salary: string | number): TeacherInterface | DirectorInterface => {
+export const createEmployee = (salary: string | number): TeacherInterface | DirectorInterface => {
     return (typeof salary === "number" && salary < 500) ? new Teacher() : new Director()
 }
 
