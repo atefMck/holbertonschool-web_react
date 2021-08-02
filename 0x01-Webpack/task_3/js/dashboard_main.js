2c0b304128fcd4beec5f270e6a8abc01b4ad275e
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-08-02 14:38:30 +0000

    fail

---
+++ 0x01-Webpack/task_3/js/dashboard_main.js
@@ -0,0 +1,12 @@
+import $ from "jquery";
+import '../css/main.css';
+const _ = require('lodash');
+
+$("<div id='logo'></div>").appendTo($('body'));
+$("<p>Holberton Dashboard</p>").appendTo($('body'));
+$("<p>Dashboard data for the students</p>").appendTo($('body'));
+$("<button>Click here to get started</button>").appendTo($('body'));
+$("<p id='count'></p>").appendTo($('body'));
+$("<p>Copyright - Holberton School</p>").appendTo($('body'));
+
+
