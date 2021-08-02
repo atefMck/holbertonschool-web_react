358fb05167d70560d217b3bfa14ce7bbaed6be83
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-08-02 09:32:35 +0000

    Init

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from "jQuery";
+
+$("<p>Holberton Dashboard</p>").appendTo($(body));
+$("<p>Dashboard data for the students</p>").appendTo($(body));
+$("Copyright - Holberton School").appendTo($(body));
