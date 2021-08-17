bd5a1e6ad378d746b1fd3b3ff07760d1c334a1b4
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-08-17 11:18:12 +0000

    Task 8 fixed

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,13 +1,17 @@
-import { Seq, Map } from 'immutable';
+import { Seq } from 'immutable';
 
 const printBestStudents = (obj) => {
   const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
-  const student = Seq(obj)
-    .fliter((subject) => subject.score < 70)
-    .map((student) => Map(student)
-      .set('firstName', capitalize(student.firstName))
-      .set('lastName', capitalize(student.lastName)));
-  console.log(student);
+  let students = Seq(obj)
+    .filter((subject) => subject.score > 70)
+    .map((student) => {
+      const newStudent = student;
+      newStudent.firstName = capitalize(student.firstName);
+      newStudent.lastName = capitalize(student.lastName);
+      return newStudent;
+    });
+  students = students.toJS();
+  console.log(students);
 };
 
 export default printBestStudents;
